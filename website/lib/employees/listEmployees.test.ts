907a5257056b08c56e6319ef3de900d0bf7b65d6
bni <bnilsson@me.com>
2025-10-11 19:16:17 +0200

    Remove sinon, instead use jest mocks

--- website/lib/employees/listEmployees.test.ts
+++ website/lib/employees/listEmployees.test.ts
@@ -1,14 +1,15 @@
 import { Employee } from "@lib/employees/employee";
 import { listEmployees } from "@lib/employees/listEmployees";
 import { Pool } from "pg";
-import sinon from "sinon";
 import testRows from "./testRows.json";
 import expectedResult from "./expectedResult.json";
 
 test("List employees", async () => {
-  sinon.stub(Pool.prototype, "query").resolves({
+  const queryResult = {
     rows: testRows
-  });
+  };
+
+  jest.spyOn(Pool.prototype, "query").mockImplementation(() => queryResult);
 
   const employees: Employee[] = await listEmployees();
 
