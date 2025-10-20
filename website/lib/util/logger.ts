9700ba68dd833b92093dd611a0bc092e5bf0f4e8
bni <bnilsson@me.com>
2025-10-20 16:16:18 +0200

    Fix logger

--- website/lib/util/logger.ts
+++ website/lib/util/logger.ts
@@ -1,39 +1,32 @@
 import pino from "pino";
-
-let environment = "local";
-if (process.env.APP_ENV) {
-  environment = process.env.APP_ENV;
-}
-
-let transport;
-if (environment === "local") {
-  transport = {
-    target: "pino-pretty",
-    options: {
-      colorize: true,
-      levelFirst: true,
-      translateTime: "yyyy-mm-dd HH:MM:ss Z"
-    }
-  };
-} else {
-  transport = {
-    target: "pino-logfmt",
-    options: {
-      flattenNestedObjects: true
-    }
-  };
-}
+import { secret } from "@lib/util/secrets";
 
 const logger = pino({
   level: process.env.PINO_LOG_LEVEL || "info",
-  formatters: {
-    level: (label) => {
-      return { level: label.toUpperCase() };
-    }
-  },
   base: undefined,
+  mixin(_context, level): { desc: string } {
+    return { desc: logger.levels.labels[level].toUpperCase() };
+  },
   timestamp: pino.stdTimeFunctions.isoTime,
-  transport: transport,
+  transport: {
+    targets: [
+      {
+        target: "pino-pretty",
+        options: {
+          colorize: true,
+          levelFirst: true,
+          translateTime: "yyyy-mm-dd HH:MM:ss Z",
+          ignore: "desc"
+        }
+      },
+      {
+        target: "pino/file",
+        options: {
+          destination: await secret("LOG_FILE_DESTINATION")
+        }
+      }
+    ]
+  },
   redact: {
     paths: ["password"]
   }
