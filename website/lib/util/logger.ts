54c15b641f81dd17248c6d7c786775e5b716c4bb
bni <bnilsson@me.com>
2025-10-21 11:57:51 +0200

    Add pino-loki

--- website/lib/util/logger.ts
+++ website/lib/util/logger.ts
@@ -3,11 +3,6 @@ import { secret } from "@lib/util/secrets";
 
 const logger = pino({
   level: process.env.PINO_LOG_LEVEL || "info",
-  base: undefined,
-  mixin(_context, level): { desc: string } {
-    return { desc: logger.levels.labels[level].toUpperCase() };
-  },
-  timestamp: pino.stdTimeFunctions.isoTime,
   transport: {
     targets: [
       {
@@ -15,14 +10,29 @@ const logger = pino({
         options: {
           colorize: true,
           levelFirst: true,
-          translateTime: "yyyy-mm-dd HH:MM:ss Z",
-          ignore: "desc"
+          translateTime: "yyyy-mm-dd HH:MM:ss Z"
         }
       },
       {
         target: "pino/file",
         options: {
           destination: await secret("LOG_FILE_DESTINATION")
+        }
+      },
+      {
+        target: "pino-loki",
+        options: {
+          batching: false,
+          labels: {
+            app: "chinook",
+            namespace: process.env.NODE_ENV || "development",
+            source: "pino",
+            runtime: `nodejs/${process.version}`
+          },
+          host: await secret("LOKI_HOST"),
+          headers: {
+            Authorization: `Bearer ${await secret("LOKI_USERNAME")}:${await secret("LOKI_TOKEN")}`
+          }
         }
       }
     ]
