f7af005add388e235edf30b7d17075bb6b086067
bni <bnilsson@me.com>
2025-11-10 14:48:28 +0100

    add gc sm

--- website/lib/util/secrets.ts
+++ website/lib/util/secrets.ts
@@ -1,7 +1,29 @@
-// Currently secrets is always read from env vars
-// Could be fetched from a key vault with TTL depending on deployment environment
+const secrets: { [index: string]: string | undefined } = {};
+
+// Put all env vars into the map
+Object.entries(process.env).map(([key, value]) => secrets[key] = value);
+
+if (process.env.GOOGLE_SECRETS_PATH) {
+  // Read additional properties (secrets) from Secret Manager (Google Cloud)
+  const { SecretManagerServiceClient } = await import("@google-cloud/secret-manager");
+
+  const client = new SecretManagerServiceClient();
+
+  const [version] = await client.accessSecretVersion({
+    name: process.env.GOOGLE_SECRETS_PATH
+  });
+
+  if (version?.payload?.data) {
+    const content = version.payload.data.toString("utf8");
+
+    console.log(`SM Content: ${content}`);
+  } else {
+    console.error(`Could not fetch secret: ${version}`);
+  }
+}
+
 const secret = async (name: string): Promise<string> => {
-  return process.env[name] || "";
+  return secrets[name] || "";
 };
 
 export { secret };
