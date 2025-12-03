3c8100c7b7aae03b41102e12a9bf189f5047316c
bni <bnilsson@me.com>
2025-12-03 14:19:13 +0100

    Read question from args

--- agent/hints.ts
+++ agent/hints.ts
@@ -1,9 +1,17 @@
 import "dotenv/config";
 import { query } from "@anthropic-ai/claude-agent-sdk";
 import { printMessage } from "./util.js";
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
+
+// npm run hints -- --question "How do I get the thing in the ice in Iceland?"
+
+const argv = yargs(hideBin(process.argv)).options({
+  question: { type: "string", demandOption: true }
+}).parseSync();
 
 for await (const message of query({
-  prompt: "Fate of Atlantis: How do I get the thing in the ice in Iceland?",
+  prompt: `${argv.question} in Fate of Atlantis`,
   options: {
     cwd: process.cwd(),
     settingSources: ["project"],
