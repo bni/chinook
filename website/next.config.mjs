5eb67dccdfe4d95f39ddfa87f3be29239abc090a
bni <bnilsson@me.com>
2025-12-08 21:05:05 +0100

    npm workspaces

--- website/next.config.mjs
+++ website/next.config.mjs
@@ -1,3 +1,9 @@
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const nextConfig = {
   devIndicators: false,
   reactStrictMode: true,
@@ -7,6 +13,9 @@ const nextConfig = {
     "/search": [
       "./node_modules/onnxruntime-node/bin/**/*"
     ]
+  },
+  turbopack: {
+    root: __dirname
   }
 };
 
