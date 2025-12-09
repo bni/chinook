14b9c41f34f7dc19f88ceb797d4ccf001779dad6
bni <bnilsson@me.com>
2025-12-09 11:43:21 +0100

    More build years lists tests

--- website/lib/artists/buildYearsList.test.ts
+++ website/lib/artists/buildYearsList.test.ts
@@ -11,3 +11,25 @@ test("Build years list", () => {
 
   expect(years).toEqual(expectedYears);
 });
+
+test("Build years list with single year", () => {
+  const fromYear = 2000;
+  const toYear = 2000;
+
+  const expectedYears: number[] = [2000];
+
+  const years: number[] = buildYearsList(fromYear, toYear);
+
+  expect(years).toEqual(expectedYears);
+});
+
+test("Build years list with fromYear after toYear", () => {
+  const fromYear = 2020;
+  const toYear = 2015;
+
+  const expectedYears: number[] = [ 2015, 2016, 2017, 2018, 2019, 2020 ];
+
+  const years: number[] = buildYearsList(fromYear, toYear);
+
+  expect(years).toEqual(expectedYears);
+});
